5a536b012e9a6650b95e0a2ea6bc8f7c1888a202
hators <hators@github.com>
2018-09-13 15:52:18 +0300

    first commit

---
+++ app/Project.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Andrei
+ * Date: 9/13/2018
+ * Time: 2:43 PM
+ */
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    //
+}
